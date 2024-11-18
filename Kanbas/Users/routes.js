338c03ca1ba42363efaddd61a49105524a305b4e
Michael <123397116+michael-neu@users.noreply.github.com>
2024-11-18 13:34:10 -0500

    Remove unused variable

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -2,8 +2,6 @@ import * as courseDao from "../Courses/dao.js";
 import * as dao from "./dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 
-let currentUser = null;
-
 export default function UserRoutes(app) {
     const createCourse = (req, res) => {
         const currentUser = req.session["currentUser"];
